14b6ce96482a4176b028ce8654c5bef27978227b
Jessica <171g0214@rcarbonifera.tecnm.mx>
2021-01-04 06:33:22 -0600

    Controlador API

--- U5_DiseñoWeb/MemoramaAPIWeb/Startup.cs
+++ U5_DiseñoWeb/MemoramaAPIWeb/Startup.cs
@@ -30,10 +30,7 @@ namespace MemoramaAPIWeb
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
